c709ba9e88a6f767f17c38d4aaba8833990e6576
kdakash25 <akashkd2525@gmail.com>
2026-03-18 16:54:17 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,12 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-global-state-demo"
-    key            = "production/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-locks"
+    bucket  = "terraform-global-state-demo"
+    key     = "production/terraform.tfstate"
+    region  = "ap-south-1"
+
+    # New locking method
+    use_lockfile = true
+
+    encrypt = true
   }
 }
