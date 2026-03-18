02bce1196e3f424b94696bb80489b2bd88cc40e6
kdakash25 <akashkd2525@gmail.com>
2026-03-18 18:17:16 +0530

    Update main.tf

--- modules/autoscaling/main.tf
+++ modules/autoscaling/main.tf
@@ -1,7 +1,7 @@
 resource "aws_launch_template" "app" {
   name_prefix   = "app-template"
   image_id      = "ami-0f58b397bc5c1f2e8"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 }
 
 resource "aws_autoscaling_group" "asg" {
