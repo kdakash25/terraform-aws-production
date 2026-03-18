57cf8cde0d6ca4ae08d9606dda825974dcff57bd
kdakash25 <akashkd2525@gmail.com>
2026-03-18 19:35:24 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -31,3 +31,15 @@ resource "aws_subnet" "private" {
     count.index
   )
 }
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+}
+resource "aws_route_table_association" "public" {
+  count = 2
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
