b3b22a61883d35026bd6208dd9dbe2a9248ebf08
kdakash25 <akashkd2525@gmail.com>
2026-03-18 17:37:11 +0530

    Create outputs.tf

---
+++ modules/vpc/outputs.tf
@@ -0,0 +1,9 @@
+output "public_subnets" {
+  description = "Public subnet IDs"
+  value       = aws_subnet.public[*].id
+}
+
+output "private_subnets" {
+  description = "Private subnet IDs"
+  value       = aws_subnet.private[*].id
+}
