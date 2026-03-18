08a80f21be51dfc1c3386ccb555ca852e228197a
kdakash25 <akashkd2525@gmail.com>
2026-03-18 18:16:15 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -8,13 +8,26 @@ resource "aws_vpc" "main" {
 
 resource "aws_subnet" "public" {
   count = 2
+
   vpc_id = aws_vpc.main.id
-  cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  cidr_block = cidrsubnet(var.vpc_cidr, 8, count.index)
+
+  availability_zone = element(
+    ["ap-south-1a", "ap-south-1b"],
+    count.index
+  )
+
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "private" {
   count = 2
+
   vpc_id = aws_vpc.main.id
-  cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index + 2)
+  cidr_block = cidrsubnet(var.vpc_cidr, 8, count.index + 10)
+
+  availability_zone = element(
+    ["ap-south-1a", "ap-south-1b"],
+    count.index
+  )
 }
